ea69a836811f40e88b5e69a4e7cf2797c2a30246
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2025-06-24 10:44:36 +0100

    Make sure the JSDoc exports for startTrace

--- src/trace/startTrace.ts
+++ src/trace/startTrace.ts
@@ -4,12 +4,6 @@ import type { ISpan } from "./types.ts";
 
 
 
-export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage | undefined, logSpan: ISpan): ISpan;
-export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage, logSpan: ISpan | undefined): ISpan;
-export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage, logSpan?: undefined): ISpan;
-export function startTrace<T extends Record<string, any> = any>(name: string, context?: T, logStorage?: undefined, logSpan?: undefined): undefined;
-export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage | undefined, logSpan: ISpan | undefined): ISpan | undefined;
-
 /**
  * Helper function to create or extend a trace. 
  * 
@@ -17,11 +11,18 @@ export function startTrace<T extends Record<string, any> = any>(name: string, co
  * Otherwise start a new trace from the provided LogStorage
  * 
  * Create a new trace from LogStorage, or if there's currently a trace extend it into a new span. 
- * @param name 
- * @param context 
- * @param logSpan 
+ * @param name The name of the new span.
+ * @param context Optional. 
+ * @param logStorage Optional. The ILogStorage from which to create a new Trace.
+ * @param logSpan Optional. An existing trace/span. Will be extended with a new span. 
  * @returns 
  */
+export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage | undefined, logSpan: ISpan): ISpan;
+export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage, logSpan: ISpan | undefined): ISpan;
+export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage, logSpan?: undefined): ISpan;
+export function startTrace<T extends Record<string, any> = any>(name: string, context?: T, logStorage?: undefined, logSpan?: undefined): undefined;
+export function startTrace<T extends Record<string, any> = any>(name: string, context: T | undefined, logStorage: ILogStorage | undefined, logSpan: ISpan | undefined): ISpan | undefined;
+
 export function startTrace<T extends Record<string, any> = any>(name: string, context?: T, logStorage?: ILogStorage, logSpan?: ISpan): ISpan | undefined {
 
     if (logSpan) {
